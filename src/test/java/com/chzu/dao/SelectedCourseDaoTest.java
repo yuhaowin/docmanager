9eb5c664b48fce01c1571e8c6c4910168c0f47e6
yuhaowin <yuhao.coding@gmail.com>
2019-03-24 11:43:45 +0800

    新增 图标字体 修改 springmvc配置文件

--- src/test/java/com/chzu/dao/SelectedCourseDaoTest.java
+++ src/test/java/com/chzu/dao/SelectedCourseDaoTest.java
@@ -7,8 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import static org.junit.Assert.*;
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:spring/applicationContext-dao.xml"})
 public class SelectedCourseDaoTest {
