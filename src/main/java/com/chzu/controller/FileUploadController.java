74d8276997c1484d0925068d766f1dbae4205e1f
yuhaowin <yuhao.coding@gmail.com>
2019-03-24 21:35:07 +0800

    新增 file 上传功能-02

--- src/main/java/com/chzu/controller/FileUploadController.java
+++ src/main/java/com/chzu/controller/FileUploadController.java
@@ -16,7 +16,7 @@ public class FileUploadController {
 
 	@RequestMapping("/upload")
     @ResponseBody
-    public String appUpload(@RequestParam MultipartFile file, HttpServletRequest request){
+    public String fileUpload(@RequestParam MultipartFile file, HttpServletRequest request){
         String basePath = request.getSession().getServletContext().getRealPath("/")+ "WEB-INF/files/";
         System.out.println("当前项目路径: " + basePath);
 
@@ -29,5 +29,10 @@ public class FileUploadController {
         System.out.println(path);
 
         return "{\"status\":200}";
+    }
+
+    @RequestMapping("/index")
+    public String showFileUploadIndex(){
+	    return "teacher/upload";
     }
 }
