a011f2cf254e968c7278906b7b0531adacb27920
allenwang321 <zbxhui@gmail.com>
2019-03-30 14:12:39 +0800

    修改关键字

--- src/main/java/com/chzu/entity/ClassSubject.java
+++ src/main/java/com/chzu/entity/ClassSubject.java
@@ -17,7 +17,7 @@ public class ClassSubject {
     private Integer courseId;
     @Column(name = "subject_name")
     private String subjectName;
-    @Column(name = "describe")
+    @Column(name = "desc")
     private String describe;
     @Column(name = "file_name")
     private String fileName;
